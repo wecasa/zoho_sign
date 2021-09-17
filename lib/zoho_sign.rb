652108821b7713c7395a7f4f692ef9b38c328eb8
Mohamed Ziata <wakematta@gmail.com>
2021-09-17 14:55:02 +0200

    :sparkles: ZohoSign is configurable

--- lib/zoho_sign.rb
+++ lib/zoho_sign.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+require "dry-configurable"
+
 require_relative "zoho_sign/version"
 
 module ZohoSign
+  extend Dry::Configurable
+
+  setting :debug, default: false
+
+  # Follow the instruction found here to generate your credentails:
+  # https://www.zoho.com/sign/api/#getting-started
+  setting :oauth do
+    setting :client_id
+    setting :client_secret
+    setting :access_token
+    setting :refresh_token
+  end
+
+  setting :api do
+    setting :domain, default: "https://sign.zoho.com"
+    setting :base_path, default: "/api/v1"
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/zoho_sign_spec.rb
+++ spec/zoho_sign_spec.rb
@@ -5,7 +5,31 @@ RSpec.describe ZohoSign do
     expect(ZohoSign::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "configurable" do
+    it "saves the configuration" do
+      described_class.config.oauth.client_id = "1000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      described_class.config.oauth.client_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+      described_class.config.update(
+        oauth: {
+          access_token: "2000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+          refresh_token: "3000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        }
+      )
+
+      expect(described_class.config.values).to eq({
+        debug: false,
+        oauth: {
+          client_id: "1000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+          client_secret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+          access_token: "2000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+          refresh_token: "3000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        },
+        api: {
+          domain: "https://sign.zoho.com",
+          base_path: "/api/v1"
+        }
+      })
+    end
   end
 end
