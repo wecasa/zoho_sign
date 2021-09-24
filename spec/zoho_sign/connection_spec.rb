2ebbc0a1679296df9e08a7ed07ba34dc7fb7b877
Mohamed Ziata <wakematta@gmail.com>
2021-09-24 17:23:56 +0200

    :fire: Specs

--- spec/zoho_sign/connection_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe ZohoSign::Connection do
-  it "todo" do
-    pending
-  end
-end
--- spec/zoho_sign/document_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe ZohoSign::Document do
-  describe ".find" do
-    it "todo" do
-      pending
-    end
-  end
-end
