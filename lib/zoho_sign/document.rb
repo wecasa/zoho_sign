d5267b92cbba36f9d0d09650443c1b46201644ff
Louise d'Amalric <louised@wecasa.fr>
2021-10-06 12:12:34 +0200

    return a temporary file

--- lib/zoho_sign/document.rb
+++ lib/zoho_sign/document.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
+require "securerandom"
 require_relative "base_record"
 
 module ZohoSign
-  # Record class to interact with Zoho Sign Documents API
   class Document < ZohoSign::BaseRecord
     class << self
-      # @param [String] :document_id Zoho Sign Document ID
-      #
-      # @return [Class] Tempfile instance
       def download_pdf(document_id)
         body = connection.download("#{request_path}/#{document_id}/pdf")
-        binding.pry
-        # Tempfile.new()
+        pdf_tmp = Tempfile.new([tempfile_name, ".pdf"], encoding: "ascii-8bit")
+        pdf_tmp.write(body)
+        pdf_tmp
       end
 
       private
@@ -24,10 +22,14 @@ module ZohoSign
       def data_key
         :requests
       end
+
+      def tempfile_name
+        SecureRandom.uuid
+      end
     end
 
-    # - Instance methods
     def download_pdf
+      self.class.download_pdf(@attributes.fetch(:request_id))
     end
   end
 end
