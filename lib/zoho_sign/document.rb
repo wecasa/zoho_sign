469832e41ab9a13660be5d783269e0f2d2624549
Sri Prasanna <k.sriprasanna@gmail.com>
2023-10-08 14:45:56 +0100

    :wrench: Added ability to fetch embedded url for template signing

--- lib/zoho_sign/document.rb
+++ lib/zoho_sign/document.rb
@@ -48,11 +48,6 @@ module ZohoSign
         ZohoSign::Document.new(**document_attributes)
       end
 
-      def get_embedded_url(request_id, action_id, host)
-        body = connection.post("#{request_path}/#{request_id}/actions/#{action_id}/embedtoken?host=#{host}")
-        JSON.parse(body, symbolize_names: true)[:sign_url]
-      end
-
       private
 
       def request_path
